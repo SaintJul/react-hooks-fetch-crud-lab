5d4f722ad560aa05e0dcbde0e6b1e910485f212b
SaintJul <favor.julius@gmail.com>
2021-06-28 02:24:54 -0400

    commit tree fd8af5c3f58ed7a70fe91fbca40f9444a4226cd6

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then(r => r.json())
+    .then(data => 
+      //console.log(data) 
+      setQuestions(data) 
+      
+    )
+  }, [])
+
+  const ques = questions.map((ques2) =>
+  <QuestionItem
+  key={ques2.id}
+  question = {ques2}
+  onDeleteClick={handleDeleteClick}
+  onAnswerChange={handleAnswerChange}
+  />)
+
+  function handleAnswerChange(id, newPost){
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "correctIndex": newPost
+      }),
+    })
+  }
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+      <ul>{ques}</ul>
+      </section>
   );
 }
 
